98d0283b1db46727090518dcbc9413fae86af5be
Вячеслав Лукин <bafana.slv@gmail.com>
2024-04-09 16:11:05 +0300

    Реализовано поключение к HH.

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from src.class_hh_api import HeadHunterAPI
+from src.class_internet_errors import Internet_connection_error
+from config import ROOT_DIR
+
+URL_GET = "https://httpbin.org/get"  # используемый адрес для отправки запроса
+
 if __name__ == '__main__':
-    pass
+    try:
+        hh_api = HeadHunterAPI(URL_GET)
+    except Internet_connection_error:
+        print('Отсутсвует подключение к сети Интернет или неверен url - адрес.')
+    else:
+        print(hh_api.get_vacancies())
+
 
